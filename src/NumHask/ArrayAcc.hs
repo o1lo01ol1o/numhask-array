39b16497b07e79848a4a4bddeb7500c0ebad1d06
Tony Day <tonyday567@gmail.com>
2017-11-11 11:46:01 +1000

    IsList and Show instances

--- src/NumHask/ArrayAcc.hs
+++ src/NumHask/ArrayAcc.hs
@@ -1,31 +1,23 @@
-{-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeInType #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wall #-}
-{-# OPTIONS_GHC -fno-warn-type-defaults #-}
 
 -- | safe-typed n-dimensional arrays with Accelerate arrays under the hood
 module NumHask.ArrayAcc where
 
+import Data.Array.Accelerate.Array.Sugar (listToShape)
+import Data.Array.Accelerate.LLVM.Native (run)
 import Data.Singletons
 import Data.Singletons.TypeLits
 import GHC.Exts
 import GHC.Show
 import NumHask.Prelude hiding (All, Map)
 import qualified Data.Array.Accelerate as A
-import qualified Data.Array.Accelerate.Array.Representation as AR (listToShape, Shape)
-import Data.Array.Accelerate.LLVM.Native (run)
 
 type family NatsToShape (ns :: [Nat]) where
   NatsToShape '[] = A.Z
@@ -38,12 +30,7 @@ type family NatsToShape (ns :: [Nat]) where
 -- >>> let a = [1..24] :: ArrayAcc '[2,3,4] Int
 -- >>> let v = [1,2,3] :: ArrayAcc '[3] Int
 -- >>> a
--- [[[1, 2, 3, 4],
---   [5, 6, 7, 8],
---   [9, 10, 11, 12]],
---  [[13, 14, 15, 16],
---   [17, 18, 19, 20],
---   [21, 22, 23, 24]]]
+-- Array (Z :. 4 :. 3 :. 2) [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 
 newtype ArrayAcc (r :: [Nat]) a = ArrayAcc (A.Acc (A.Array (NatsToShape r) a))
 
@@ -55,11 +42,10 @@ instance
     ( SingI r
     , Num a
     , A.Elt a
-    , AR.Shape (NatsToShape r)
     , A.Shape (NatsToShape r)
     ) => IsList (ArrayAcc (r :: [Nat]) a) where
   type Item (ArrayAcc r a) = a
-  fromList l = ArrayAcc $ A.use $ A.fromList (AR.listToShape sh) l
+  fromList l = ArrayAcc $ A.use $ A.fromList (listToShape sh) l
     where
       sh = fmap fromIntegral (fromSing (sing :: Sing r))
   toList (ArrayAcc a) = A.toList $ run a
@@ -71,3 +57,4 @@ instance
     , A.Shape (NatsToShape r)
     ) => Show (ArrayAcc r a) where
   show (ArrayAcc l) = GHC.Show.show $ run l
+
